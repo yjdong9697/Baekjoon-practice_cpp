1955ceba5e7cb1b11f6c1c090fb45b868fbb42ca
yjdong9697 <yjdong9697@yonsei.ac.kr>
2020-08-16 03:08:16 +0900

    2020.08.16 ps

---
+++ baekjoon_1330.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int A, B;
+    cin >> A >> B;
+
+    if (A > B){
+        cout << ">\n";
+    }
+
+    else if (A < B){
+        cout << "<\n";
+    }
+
+    else{
+        cout << "==\n";
+    }
+
+    return 0;
+
+}
