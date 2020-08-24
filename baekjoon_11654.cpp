d8852902a6c32c1eb69b0f55e75d4c2626ae1a24
yjdong9697 <yjdong9697@yonsei.ac.kr>
2020-08-24 16:40:51 +0900

    2020.08.24 ps

---
+++ baekjoon_11654.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    char input_value;
+    cin >> input_value;
+    cout << int(input_value) << "\n";
+
+    return 0;
+}
