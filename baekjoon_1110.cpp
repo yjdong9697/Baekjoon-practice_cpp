60b7fae92b5fda6cf25cd319f8db79424f14288f
yjdong9697 <yjdong9697@yonsei.ac.kr>
2020-09-02 16:38:10 +0900

    2020.09.02 ps

---
+++ baekjoon_1110.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    int n;
+    cin >> n;
+    int temp = n;
+    int count = 0;
+
+    int first = n / 10;
+    int second = n % 10;
+    int sum = (first + second) % 10;
+
+    n = second * 10 + sum;
+
+    count += 1; // first cycle
+
+    while (n != temp){
+        int first = n / 10;
+        int second = n % 10;
+        int sum = (first + second) % 10;
+
+        n = second * 10 + sum;
+        count += 1;
+    }
+
+    cout << count << "\n";
+
+    return 0;
+
+}
