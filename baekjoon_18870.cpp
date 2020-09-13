875a5ae7ebed284b66bf187a70ed4943f2948b14
yjdong9697 <yjdong9697@yonsei.ac.kr>
2020-09-14 00:35:30 +0900

    2020.09.14 ps

---
+++ baekjoon_18870.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(void){
+    int n;
+    cin >> n;
+    vector<long long> data;
+    for(int i = 0; i < n; i++){
+        int value;
+        cin >> value;
+        data.push_back(value);
+    }
+
+    vector<long long> data_save = data;
+    sort(data.begin(), data.end());
+    data.erase(unique(data.begin(), data.end()), data.end());
+    
+    int size_of_data = data.size();
+
+    for(int i = 0; i<n; i++){
+        int start = 0;
+        int end = size_of_data;
+        int mid = (start + end) / 2;
+
+        // Binary search
+        while(start != mid){
+            mid = (start + end) / 2;
+            if (data[mid] > data_save[i]){
+                end = mid - 1;
+            }
+            else if(data[mid] < data_save[i]){
+                start = mid + 1;
+            }
+            else break;
+        }
+        cout << mid << " ";
+    }
+    
+    cout << "\n";
+
+    return 0;
+}
