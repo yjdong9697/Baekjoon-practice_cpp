26976927b7af63b471fb2c42059c2233e376d732
yjdong9697 <yjdong9697@yonsei.ac.kr>
2020-09-16 22:37:23 +0900

    2020.09.16 ps

---
+++ baekjoon_11650.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+typedef struct{
+    int x;
+    int y;
+}point;
+
+bool compare(const point value1, const point value2){
+    if (value1.x == value2.x){
+        return value1.y < value2.y;
+    }
+    else{
+        return value1.x < value2.x;
+    }
+
+}
+
+int main(void){
+    int n;
+    cin >> n;
+
+    vector<point> cor_store;
+    for(int i = 0; i < n; i++){
+        point temp;
+        cin >> temp.x >> temp.y;
+        cor_store.push_back(temp);
+    }
+
+    sort(cor_store.begin(), cor_store.end(), compare);
+
+    for(int i = 0; i < n ; i++){
+        cout << cor_store[i].x << " " << cor_store[i].y << "\n";
+    }
+
+    return 0;
+
+}
