fd71a6049b463ffcd52b4ceeae1a32aad368cbf7
yjdong9697 <yjdong9697@yonsei.ac.kr>
2021-01-04 20:48:23 +0900

    2020.01.04 ps

---
+++ baekjoon_12970.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    int N, K;
+    cin >> N >> K;
+
+    int max = (N / 2) * N;
+    if(K > max){
+        cout << "-1\n"; // 불가능한 상황
+    }
+    else{
+        
+    }
+}
