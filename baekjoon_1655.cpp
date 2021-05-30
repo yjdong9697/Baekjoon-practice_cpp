7ad3e59f8441aad87450d53a293e01693d15cf24
yjdong9697 <yjdong9697@yonsei.ac.kr>
2021-05-30 19:28:57 +0900

    2021.05.30 ps (Priority Queue)

--- baekjoon_1655.cpp
+++ baekjoon_1655.cpp
@@ -1,51 +1,47 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <queue>
-#include <stack>
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 
 using namespace std;
 
 int main(void){
+    fastio;
     int n;
     cin >> n;
-    priority_queue<int, vector<int>, greater<int> >pq;
+    
+    priority_queue<int, vector<int>, less<int> > l;
+    priority_queue<int, vector<int>, greater<int> > r;
 
-    for(int i = 0; i < n; i++){
-        int temp;
-        cin >> temp;
-        if(i % 2 == 0){
-            pq.push(temp);
-            vector<int> junk_store;
-            for(int j = 0; j < (i / 2); j++){
-                junk_store.push_back(pq.top());
-                pq.pop();
-            }
-            cout << pq.top() << "\n";
-            for(int i = 0; i < junk_store.size(); i++){
-                pq.push(junk_store[i]);
-            }
+    // First input
+    int temp;
+    cin >> temp;
+    l.push(temp);
+    cout << temp << "\n";
+
+    for(int i = 2; i <= n; i++){
+        int t;
+        cin >> t;
+
+        // 일단 왼쪽보다 작거나 같으면 왼쪽에다가 넣음
+        if(l.top() >= t){
+            l.push(t);
         }
-        else if(i % 2 == 1){
-            pq.push(temp);
-            vector<int> junk_store;
-            for(int j = 0; j < (i / 2); j++){
-                junk_store.push_back(pq.top());
-                pq.pop();
-            }
-            int check1 = pq.top();
-            pq.pop();
-            int check2 = pq.top();
-            cout << min(check1, check2) << "\n";
-            for(int i = 0; i < junk_store.size(); i++){
-                pq.push(junk_store[i]);
-            }
-            pq.push(check1);
-            pq.push(check2);
+        else{
+            r.push(t);
+        }
 
+        // 빼고 정리하는 작업
+        if(l.size() == (i / 2) + (i % 2)){
+                cout << l.top() << "\n";
+        }
+        else if(l.size() > (i / 2) + (i % 2)){
+            r.push(l.top());
+            l.pop();
+            cout << l.top() << "\n";
+        }
+        else{
+            l.push(r.top());
+            r.pop();
+            cout << l.top() << "\n";
         }
     }
-
-    return 0;
 }
