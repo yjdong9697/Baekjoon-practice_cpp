883705d65012ceb0564f8878bb37582316171110
yjdong9697 <yjdong9697@yonsei.ac.kr>
2021-10-24 22:42:00 +0900

    2021.10.24 ps (Backtracking)

---
+++ baekjoon_15657.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+vector<int> v;
+int N, M;
+int result[8];
+
+void solve(int cur, int index)
+{
+    result[index++] = v[cur];
+    if (index == M)
+    {
+        for (int i = 0; i < M; i++)
+        {
+            if (i != M - 1)
+                cout << result[i] << " ";
+            else
+                cout << result[i] << "\n";
+        }
+        return;
+    }
+    for (int i = cur; i < N; i++)
+    {
+        solve(i, index);
+    }
+    return;
+}
+
+int main(void)
+{
+    fastio;
+    memset(result, 0, sizeof(result));
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        int t;
+        cin >> t;
+        v.push_back(t);
+    }
+
+    sort(v.begin(), v.end());
+    for (int i = 0; i < N; i++)
+    {
+        solve(i, 0);
+    }
+    return 0;
+}
