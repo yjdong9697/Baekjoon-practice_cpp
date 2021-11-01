71c38db0f1cafe59f41db59cfb61e0fe2fd4295f
yjdong9697 <yjdong9697@yonsei.ac.kr>
2021-11-01 19:45:29 +0900

    2021.11.01 ps (DP)

---
+++ baekjoon_9655.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int dp[1001];
+int n;
+
+int dp_value(int x){
+    if(x < 1) return 987654321;
+    if(dp[x] != -1) return dp[x];
+    else{
+        return dp[x] = min(dp_value(x - 1), dp_value(x - 3)) + 1;
+    } 
+}
+
+int main(void){
+    memset(dp, -1, sizeof(dp));
+    dp[1] = 1;
+    dp[3] = 1;
+    cin >> n;
+    
+    int check = dp_value(n);
+    if(check % 2 == 1){
+        cout << "SK\n";
+    }
+    else{
+        cout << "CY\n";
+    }
+    return 0;
+}
