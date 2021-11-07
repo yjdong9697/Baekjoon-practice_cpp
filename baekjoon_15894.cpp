e505bb0f6ccb3c63b1192055a6e7008e9b2110f4
yjdong9697 <yjdong9697@yonsei.ac.kr>
2021-11-07 16:25:52 +0900

    2021.11.07 ps

---
+++ baekjoon_15894.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main(void){
+    long long n;
+    cin >> n;
+    cout << n * 4 << "\n";
+    return 0;
+}
