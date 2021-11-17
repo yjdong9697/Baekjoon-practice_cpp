ceba9458fe25ed5ee8e197099191e2558dad9a3f
yjdong9697 <yjdong9697@yonsei.ac.kr>
2021-11-18 08:30:42 +0900

    2021.11.18 ps

---
+++ baekjoon_16430.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    int a, b;
+    cin >> a >> b;
+    
+    cout << (b - a) << " " << b << "\n";
+    return 0;
+}
