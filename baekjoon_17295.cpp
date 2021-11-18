422a613f7716566f5cbba60e903291b30ad342b6
yjdong9697 <yjdong9697@yonsei.ac.kr>
2021-11-18 14:45:24 +0900

    2021.11.18 ps

---
+++ baekjoon_17295.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    cout << "Avengers: Endgame\n";
+    return 0;
+}
