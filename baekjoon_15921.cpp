484e09f26f08459d169928e0d653ccc854104e71
yjdong9697 <yjdong9697@yonsei.ac.kr>
2021-12-31 12:31:42 +0900

    2021.12.31 ps

---
+++ baekjoon_15921.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int t;
+        cin >> t;
+    }
+
+    if(n == 0){
+        cout << "divide by zero\n";
+    }
+    else{
+        cout << "1.00" << "\n";
+    }
+    return 0;
+}
