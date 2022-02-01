ef557945b4daa4962a89f054d8947508bb73e5f2
yjdong9697 <yjdong9697@yonsei.ac.kr>
2022-02-01 21:31:28 +0900

    2022.2.1. ps

---
+++ baekjoon_14264.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    cout << fixed;
+    cout.precision(10);
+    double n;
+    cin >> n;
+    cout << (n * n * sqrt(3)) / 4 << "\n";
+    return 0;
+}
