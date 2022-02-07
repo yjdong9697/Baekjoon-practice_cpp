4e52d54c631738c22db7a023280eddeb80ea5f77
yjdong9697 <yjdong9697@yonsei.ac.kr>
2022-02-07 22:30:31 +0900

    2022.2.7 ps

---
+++ baekjoon_20499.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+
+int move_x[4] = {-1, 1, 0, 0};
+int move_y[4] = {0, 0, -1, 1};
+
+
+int main() {
+    fastio;
+    string s;
+    cin >> s;
+    for(int i = 0; i < 2; i++){
+        s.replace(s.find("/"), 1, " ");
+    }
+    stringstream in(s);
+    
+    int k, d, a;
+    in >> k >> d >> a;
+    if(k + a < d || d == 0){
+        cout << "hasu\n";
+    }
+    else cout << "gosu\n";
+    return 0;
+}
