4a72a22ee669fcfaea5dbeb40da4730dba69f992
yjdong9697 <yjdong9697@yonsei.ac.kr>
2022-02-09 16:40:11 +0900

    2022.2.9 ps (Rabin-karp)

--- baekjoon_10538.cpp
+++ baekjoon_10538.cpp
@@ -9,22 +9,28 @@ using tiii = tuple<int, int, int>;
 int move_x[4] = {-1, 1, 0, 0};
 int move_y[4] = {0, 0, -1, 1};
 const int MAX_SIZE = 2000;
+const ll MOD = 1e+9;
 
-int row_hash[MAX_SIZE][MAX_SIZE]; // 가로줄 해싱값
-int hash_store[MAX_SIZE][MAX_SIZE]; // 최정 해싱값
+ll row_hash[MAX_SIZE][MAX_SIZE]; // 가로줄 해싱값
+ll hash_store[MAX_SIZE][MAX_SIZE]; // 최정 해싱값
+
+ll mod(ll v){
+	if(v >= 0) return v % MOD;
+	else return ((-v / MOD + 1) * MOD + v) % MOD;
+}
 
 // 가로 방향으로 해싱
 void RowHashFind(string S, int m, int line) {
 	int n = S.length();
-	int power = 1, hash_value = 0;
+	ll power = 1, hash_value = 0;
 	for (int i = 0; i <= n - m; i++) {
         if(i == 0){
             for(int j = 0; j < m; j++){
-                hash_value = hash_value * 37 + S[i + j];
-                if(j != m - 1) power = power * 37;
+                hash_value = mod(mod(hash_value * 37) + S[i + j]);
+                if(j != m - 1) power = mod(power * 37);
             }
         }
-        else hash_value = 37 * (hash_value - S[i - 1] * power) + S[i + m - 1];
+        else hash_value = mod(mod(37 * mod(hash_value - mod(S[i - 1] * power))) + S[i + m - 1]);
         row_hash[line][i] = hash_value;
 	}
 }
@@ -32,15 +38,15 @@ void RowHashFind(string S, int m, int line) {
 // 가로 방향으로 해싱한 것을 다시 세로 방향으로 해싱
 void ColHashFind(int H, int W, int N, int M) {
 	for (int i = 0; i <= M - W; i++) {
-		int hash_value = 0, power = 1;
+		ll hash_value = 0, power = 1;
 		for (int k = 0; k <= N - H; k++) {
 			if (k == 0) {
 				for (int t = 0; t < H; t++) {
-					hash_value = hash_value + row_hash[H - t - 1][i] * power;
-					if (t != H - 1) power = power * 5831;
+					hash_value = mod(hash_value + mod(row_hash[H - t - 1][i] * power));
+					if (t != H - 1) power = mod(power * 5831);
 				}
 			}
-			else hash_value = 5831 * (hash_value - row_hash[k - 1][i] * power) + row_hash[k+H - 1][i];
+			else hash_value = mod(mod(5831 * mod((hash_value - mod(row_hash[k - 1][i] * power)))) + row_hash[k+H - 1][i]);
 			hash_store[k][i] = hash_value;
 		}
 	}
@@ -51,24 +57,25 @@ int main() {
 	int H, W, N, M;
 	cin >> H >> W >> N >> M;
 	string temp;
-	for (int i = 0; i < H; i++) {
+	for (int i = 0; i < H; i++){
 		cin >> temp;
 		RowHashFind(temp, W, i);
 	}
-    ColHashFind(H, W, H, W);
-    int cmp = hash_store[0][0];
+	ColHashFind(H, W, H, W);
+	ll cmp = hash_store[0][0];
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++){
 		cin >> temp;
 		RowHashFind(temp, W, i);
 	}
-	ColHashFind(H,W,N,M);
-		
-    int ret = 0;
-	for (int i = 0; i <= N - H; i++)
-		for (int j = 0; j <= M - W; j++)
+	ColHashFind(H, W, N, M);
+
+	ll ret = 0;
+	for (int i = 0; i <= N - H; i++){
+		for (int j = 0; j <= M - W; j++){
 			if (hash_store[i][j] == cmp) ret++;
-			
+		}
+	}
 	cout << ret << "\n";
 	return 0;
 }
