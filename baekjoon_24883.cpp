eaed8a583f8cb795cfc343a20c1d4aee3d2ef5b7
yjdong9697 <yjdong9697@yonsei.ac.kr>
2022-04-28 15:43:41 +0900

    2022.4.28 ps

---
+++ baekjoon_24883.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    char t;
+    cin >> t;
+    if(t == 'N' || t == 'n'){
+        cout << "Naver D2\n";
+    }
+    else cout << "Naver Whale\n";
+    return 0;
+}
