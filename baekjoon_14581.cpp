2338b7ae51766fe61cf2d0f973aa3e838f8e80fe
yjdong9697 <yjdong9697@yonsei.ac.kr>
2022-06-30 12:22:21 +0900

    2022.6.30 ps

---
+++ baekjoon_14581.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+
+int move_x[4] = {-1, 1, 0, 0};
+int move_y[4] = {0, 0, -1, 1};
+
+int main() {
+    fastio;
+    string t;
+    cin >> t;
+    cout << ":fan::fan::fan:" << "\n";
+    cout << ":fan::" << t << "::fan:" << "\n";
+    cout << ":fan::fan::fan:" << "\n";
+    return 0;
+}
